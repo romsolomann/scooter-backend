15abefdbbbef5b038c8eaf1bfce523d66ee8b108
romsolomann <rom@propdo.com>
2023-08-19 20:39:17 +0300

    cors

--- server.ts
+++ server.ts
@@ -55,6 +55,7 @@ if (process.env.NODE_ENV === "production") {
       "http://localhost:8080",
       "http://127.0.0.1:3000",
       "http://localhost:3000",
+      "https://scooter-next-js-f19947913171.herokuapp.com",
     ],
     credentials: true,
   };
